25d3c48def9a65dbf512d7bc63fa39e39cc2dac6
mironbanjac-abh <miron.banjac@atlantbh.com>
2025-09-11 15:18:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List
-from statsmodels.tsa.arima.model import ARIMA
-import numpy as np
 import pandas as pd
 
 from model import arima
@@ -12,6 +10,8 @@ app = FastAPI()
 class ForecastRequest(BaseModel):
     scores: List[float]
     window_size: int = 5
+    CI_method: str = "t_distribution"
+
 
 @app.post("/forecast")
 def forecast_arima(request: ForecastRequest):
@@ -24,10 +24,10 @@ def forecast_arima(request: ForecastRequest):
             )
     
     # Validate window_size is smaller than the number of scores
-    if request.window_size >= len(request.scores):
+    if request.window_size > len(request.scores):
         raise HTTPException(
             status_code=400,
-            detail=f"window_size ({request.window_size}) must be smaller than the number of scores ({len(request.scores)})"
+            detail=f"window_size ({request.window_size}) must be smaller than or equal to the number of scores ({len(request.scores)})"
         )
     
     # Validate window_size is positive
@@ -37,9 +37,17 @@ def forecast_arima(request: ForecastRequest):
             detail=f"window_size must be a positive integer"
         )
     
+    # Validate CI_method is one of the supported options
+    if request.CI_method != "t_distribution" and request.CI_method != "normal_distribution" and request.CI_method != "arima_distribution":
+        raise HTTPException(
+            status_code=400,
+            detail=f"CI_distribution must be a supported type. (t_distribution or normal_distribution or arima_distribution)"
+        )
+
+
     try:
         df = pd.DataFrame(request.scores, columns=['score_percentage'])
-        result = arima(df, WINDOW_SIZE=request.window_size)
+        result = arima(df, WINDOW_SIZE=request.window_size, CI_method = request.CI_method)
         return result
 
     except Exception as e:
