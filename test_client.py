68ee81d268b86e093f9b2646a8505e62bfeeda1f
mironbanjac-abh <miron.banjac@atlantbh.com>
2025-09-11 15:12:33 +0200

    Update test_client.py

--- test_client.py
+++ test_client.py
@@ -2,8 +2,9 @@
 import requests
 
 payload = {
-    "scores": [0.9, 0.88, 0.75, 0.78, 0.74, 0.69, 0.95, 0.88, 0.9, 0.63, 0.93, 0.95, 0.94, 0.56, 0.76, 0.99, 0.89, 0.99, 0.93, 0.99],
-    "window_size": 10
+    "scores": [0.9, 0.88, 0.75, 0.78, 0.74, 0.69],
+    "window_size": 5,
+    "CI_method": "t_distribution"
 }
 
 response = requests.post("http://127.0.0.1:8000/forecast", json=payload)
